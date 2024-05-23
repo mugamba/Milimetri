fdccdd1cb242666a1e674feae65290e5abcf924f
Mate Sosa <mate.sosa@pakel.hr>
2024-05-23 12:33:58 +0200

    Initial Commit

---
+++ MilimetriApp/MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace MilimetriApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonSelectFile = new Button();
+            textBoxSelectedFile = new TextBox();
+            textBoxFolder = new TextBox();
+            buttonOdaberiFolder = new Button();
+            buttonPokreniObradu = new Button();
+            SuspendLayout();
+            // 
+            // buttonSelectFile
+            // 
+            buttonSelectFile.Location = new Point(12, 14);
+            buttonSelectFile.Name = "buttonSelectFile";
+            buttonSelectFile.Size = new Size(175, 23);
+            buttonSelectFile.TabIndex = 0;
+            buttonSelectFile.Text = "Odaberi ulaznu datoteku";
+            buttonSelectFile.UseVisualStyleBackColor = true;
+            buttonSelectFile.Click += buttonOdaberiDatoteku_Click;
+            // 
+            // textBoxSelectedFile
+            // 
+            textBoxSelectedFile.Location = new Point(193, 14);
+            textBoxSelectedFile.Name = "textBoxSelectedFile";
+            textBoxSelectedFile.Size = new Size(468, 23);
+            textBoxSelectedFile.TabIndex = 1;
+            // 
+            // textBoxFolder
+            // 
+            textBoxFolder.Location = new Point(193, 43);
+            textBoxFolder.Name = "textBoxFolder";
+            textBoxFolder.Size = new Size(468, 23);
+            textBoxFolder.TabIndex = 2;
+            // 
+            // buttonOdaberiFolder
+            // 
+            buttonOdaberiFolder.Location = new Point(12, 42);
+            buttonOdaberiFolder.Name = "buttonOdaberiFolder";
+            buttonOdaberiFolder.Size = new Size(175, 23);
+            buttonOdaberiFolder.TabIndex = 3;
+            buttonOdaberiFolder.Text = "Odaberi izlazni direktorij";
+            buttonOdaberiFolder.UseVisualStyleBackColor = true;
+            buttonOdaberiFolder.Click += buttonOdaberiFolder_Click;
+            // 
+            // buttonPokreniObradu
+            // 
+            buttonPokreniObradu.Location = new Point(486, 210);
+            buttonPokreniObradu.Name = "buttonPokreniObradu";
+            buttonPokreniObradu.Size = new Size(175, 23);
+            buttonPokreniObradu.TabIndex = 4;
+            buttonPokreniObradu.Text = "Pokreni obradu";
+            buttonPokreniObradu.UseVisualStyleBackColor = true;
+            buttonPokreniObradu.Click += buttonPokreniObradu_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(694, 254);
+            Controls.Add(buttonPokreniObradu);
+            Controls.Add(buttonOdaberiFolder);
+            Controls.Add(textBoxFolder);
+            Controls.Add(textBoxSelectedFile);
+            Controls.Add(buttonSelectFile);
+            Name = "MainForm";
+            Text = "Milimetar app";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonSelectFile;
+        private TextBox textBoxSelectedFile;
+        private TextBox textBoxFolder;
+        private Button buttonOdaberiFolder;
+        private Button buttonPokreniObradu;
+    }
+}
---
+++ MilimetriApp/MainForm.cs
@@ -0,0 +1,167 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MilimetriApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOdaberiFolder_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    textBoxFolder.Text = fbd.SelectedPath;
+                }
+            }
+
+        }
+
+        private void buttonOdaberiDatoteku_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new OpenFileDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.FileName))
+                {
+                    textBoxSelectedFile.Text = fbd.FileName;
+                }
+            }
+        }
+
+        private void buttonPokreniObradu_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(textBoxFolder.Text) || !File.Exists(textBoxSelectedFile.Text))
+            {
+                MessageBox.Show("Odabrana datoteka ili direktorij nisu ispravni!");
+            }
+            else
+            { 
+            
+                var lines = File.ReadAllLines(textBoxSelectedFile.Text);
+
+                var list = new List<MilimetriData>();
+                var provider = new CultureInfo("en-EN");
+
+                foreach (var line in lines)
+                {
+
+                    var mili = new MilimetriData();
+
+                   var splits =  line.Split(",");
+                   
+                    if (!String.IsNullOrEmpty(splits[0]))
+                        mili.Atribut1 = decimal.Parse(splits[0], System.Globalization.NumberStyles.AllowDecimalPoint, provider);
+                    if (!String.IsNullOrEmpty(splits[1]))
+                        mili.Atribut2 = decimal.Parse(splits[1], System.Globalization.NumberStyles.AllowDecimalPoint, provider);
+                    if (!String.IsNullOrEmpty(splits[2]))
+                        mili.X = decimal.Parse(splits[2], System.Globalization.NumberStyles.AllowDecimalPoint, provider);
+                    if (!String.IsNullOrEmpty(splits[3]))
+                        mili.Y = decimal.Parse(splits[3], System.Globalization.NumberStyles.AllowDecimalPoint, provider);
+
+                    list.Add(mili);
+                }
+
+
+                var test = list.Count;
+
+                var grouped = list.GroupBy(o => new { o.X, o.Y }).Select(g => g).ToList();
+
+
+                var returnListMilimeti = new List<MilimetriData>();
+                var errorList = new List<MilimetriData>();
+
+                foreach (var group in grouped)
+                {
+                    var returnData = new MilimetriData();
+
+                    returnData.X = group.Key.X;
+                    returnData.Y = group.Key.Y;
+
+
+                    var listAtt1 = list.Where(o => o.X == group.Key.X && o.Y == group.Key.Y && o.Atribut1 != null).ToList();
+                    if (listAtt1.Count > 1)
+                        errorList.AddRange(listAtt1);
+                    else
+                    {
+                        if (listAtt1.Count == 1)
+                        {
+                            returnData.Atribut1 = listAtt1.Select(o => o.Atribut1).FirstOrDefault();
+                        }
+                    
+                    }
+
+                    var listAtt2 = list.Where(o => o.X == group.Key.X && o.Y == group.Key.Y && o.Atribut2 != null).ToList();
+                    if (listAtt2.Count > 1)
+                        errorList.AddRange(listAtt2);
+                    else
+                    {
+                        if (listAtt1.Count == 1)
+                        {
+                            returnData.Atribut2 = listAtt2.Select(o => o.Atribut2).FirstOrDefault();
+                        }
+
+                    }
+
+                    returnListMilimeti.Add(returnData);
+                }
+
+
+                var builder1 = new StringBuilder();
+                foreach (var tt in returnListMilimeti)
+                    builder1.AppendLine(String.Format(CultureInfo.InvariantCulture, "{0:######################.######################}," +
+                        "{1:######################.######################}," +
+                        "{2:######################.######################}," +
+                        "{3:######################.######################}", tt.Atribut1, tt.Atribut2, tt.X, tt.Y));
+
+
+                var builder2 = new StringBuilder();
+                foreach (var tt in errorList)
+                    builder2.AppendLine(String.Format(CultureInfo.InvariantCulture, "{0:######################.######################}," +
+                        "{1:######################.######################}," +
+                        "{2:######################.######################}," +
+                        "{3:######################.######################}", tt.Atribut1, tt.Atribut2, tt.X, tt.Y));
+
+                FileInfo v = new FileInfo(textBoxSelectedFile.Text);
+
+                File.WriteAllText(textBoxFolder.Text + "\\" + v.Name.Replace(v.Extension, "") + "_Obrada_" + DateTime.Today.ToString("yyyyMMdd")+v.Extension , builder1.ToString());
+                
+                if (builder2.Length == 0)
+                    File.WriteAllText(textBoxFolder.Text + "\\" + v.Name.Replace(v.Extension, "") + "_Log_" + DateTime.Today.ToString("yyyyMMdd") + v.Extension, "Nema neispravnih stavki!" );
+                else
+                    File.WriteAllText(textBoxFolder.Text + "\\" + v.Name.Replace(v.Extension, "") + "_Log_" + DateTime.Today.ToString("yyyyMMdd") + v.Extension,  builder2.ToString());
+
+
+                var message = builder2.Length == 0 ? "Uspješno je obrađena datoteka, nije bilo pogrešnih unosa, pogledaj u izlazni direktorij!" :
+                    "Ima pogrešnih upisa, pogledaj u izlazni direktorij u Log datoteku.";
+
+                MessageBox.Show(message);
+            }
+        }
+    }
+
+    public class MilimetriData
+    { 
+        public Decimal X { get; set; }
+        public Decimal Y { get; set; }
+        public Decimal? Atribut1 { get; set; }
+        public Decimal? Atribut2 { get; set; }
+
+    }
+
+
+
+
+
+
+}
